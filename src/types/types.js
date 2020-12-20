ee4e84bd7f249d3e6c9bb1c25b491cf68cfff18d
EduardoMay <eduardo_may@outlook.com>
2020-12-20 13:34:58 -0500

    creando los tipos

--- src/types/types.js
+++ src/types/types.js
@@ -6,4 +6,8 @@ export const types = {
 	activeNewNote: '[ActiveNewNote] nueva nota',
 	desactivateNewNote: '[DesactivateNewNote] nueva nota',
 	updateNote: '[UpdateNote] actualizar nota',
+
+	addCategorie: '[AddCategorie] agregar categorie',
+	updatedCategorie: '[UpdateCategorie] actualizar categorie',
+	deleteCategorie: '[DeleteCategorie] eliminar categorie',
 };
